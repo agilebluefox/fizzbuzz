7d059faab5c5c96636447a3a2a81bf49bc76bdb6
David Conner <agilebluefox@gmail.com>
2016-05-30 09:45:59 -0400

    Refactored to move fizzbuzz test into a named function, add an input box, include some rudimentary validation, and just enough css to see the results on the screen.

--- js/script.js
+++ js/script.js
@@ -4,15 +4,53 @@
 */
 
 $(document).ready( function() {
-    for (var i=1; i <= 100; i++) {
+    // When the button is pressed, clear the container for the printout,
+    // get the input, make sure it's a valid number
+    var number;
+    $('#inputNumber').submit(function(e) {
+        $('div.result').empty();
+        number = $('#inputNumber input').val();
+        e.preventDefault();
+        this.reset();
+        number = validateInput(Number(number));
+    });
+});
+
+function validateInput(userNumber) {
+    // Set the warning string to false
+    console.log(warning);
+    var warning = "";
+
+    // If the number is valid, print the analysis and conversion
+    // If not, present some validation messages
+    if ( userNumber >= 15 && userNumber <= 1500 ) {
+        $('div.result p').replaceWith('<p>' + fizzBuzz(userNumber) + '</p>');
+    } else if ( userNumber < 15 ) {
+        warning = "Your number is too low.";
+    } else if ( userNumber > 1500 ) {
+        warning = "Your number is too high.";
+    } else {
+        warning = "Please enter a valid number.";
+    }
+
+    // If a warning message exists, print it to the results container
+    if (warning) {
+        console.log(warning);
+        $('div.result p').replaceWith('<p>' + warning + '</p>');
+    }
+}
+
+// This function takes an integer and performs the 'fizzbuzz' test
+function fizzBuzz(maxInteger) {
+    for (var i=1; i <= maxInteger; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            $('body').append('<p>FizzBuzz</p>');
+            $('div.result').append('<p>FizzBuzz</p>');
         } else if (i % 3 === 0) {
-            $('body').append('<p>Fizz</p>');
+            $('div.result').append('<p>Fizz</p>');
         } else if (i % 5 === 0) {
-            $('body').append('<p>Buzz</p>');
+            $('div.result').append('<p>Buzz</p>');
         } else {
-            $('body').append('<p>' + i + '</p>');
+            $('div.result').append('<p>' + i + '</p>');
         }
     }
-});
+}
