5b80895aef9ab0b46eb7489bdf9a2962497b9f41
David Conner <agilebluefox@gmail.com>
2016-05-30 10:52:33 -0400

    Improved comments and fixed an omission on a variable declaration.

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@
 
 $(document).ready( function() {
     // When the button is pressed, clear the container for the printout,
-    // get the input, make sure it's a valid number, then perform the test
+    // get the input, make sure it's a valid number, then perform the test.
     var number;
+
     $('#inputNumber').submit(function(e) {
+        // Included this to clear the results container when a new
+        // input value was submitted.
         $('div.result').empty();
         number = $('#inputNumber input').val();
         e.preventDefault();
         this.reset();
-        validNumber = validateInput(Number(number));
+        var validNumber = validateInput(Number(number));
         if (validNumber) {
             fizzBuzz(validNumber);
         }
@@ -23,7 +26,7 @@ $(document).ready( function() {
         var warning = "";
 
         // If the number is valid, print the analysis and conversion
-        // If not, present some validation messages
+        // If not, present some validation messages.
         if ( userNumber >= 15 && userNumber <= 1500 ) {
             return userNumber;
         } else if ( userNumber < 15 ) {
@@ -37,14 +40,14 @@ $(document).ready( function() {
     }
 
     function printMessage(message) {
-        // If a warning message exists, print it to the results container
+        // If a warning message exists, print it to the results container.
         if (message) {
             console.log(message);
             $('div.result').append('<p>' + message + '</p>');
         }
     }
 
-    // This function takes an integer and performs the 'fizzbuzz' test
+    // This function takes an integer and performs the 'fizzbuzz' test.
     function fizzBuzz(maxInteger) {
         for (var i=1; i <= maxInteger; i++) {
             if (i % 3 === 0 && i % 5 === 0) {
